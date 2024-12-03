c86e8158e347fe104bcec5a40b7bfb05efda6c4a
Alba Mendez <me@alba.sh>
2024-12-03 01:13:24 +0100

    more boilerplate

--- day1/code.hs
+++ day1/code.hs
@@ -1,7 +1,16 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# LANGUAGE ViewPatterns, TypeApplications #-}
+
 main = getContents >>= (print . parts . lines)
 
 parts x = (part1 x, part2 x)
 
+
+-- PART 1
+
 part1 x = 0
+
+
+-- PART 2
 
 part2 x = 0
